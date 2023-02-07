12d359e6134c66342afd4d9ef7756ecc558152f7
Nicholas Lee <46840082+exodecode@users.noreply.github.com>
2023-02-07 16:46:07 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# NAME=${1?Error: no name given}
+# echo "Hello! $NAME"
+LINK=${1?Error: no playlist url given}
+wget $LINK -O out.html
+# touch pretty.html
+tidy out.html > pretty.html
+
+python scraper.py
