8ccfa0844818a36935ea683206fda9df6e3b9abf
Nicholas Lee <46840082+exodecode@users.noreply.github.com>
2023-02-13 01:15:21 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# NAME=${1?Error: no name given}
-# echo "Hello! $NAME"
-LINK=${1?Error: no playlist url given}
-wget $LINK -O src.html
-# touch pretty.html
-#tidy out.html > pretty.html
 
-python scraper.py
+LINK=${1?Error: no playlist url given}
+wget -q -O - $LINK | grep -oP '(, )?<a href="/artist/.*?">.*?</a>|<a .*? href="/track/.*?">.+?</a>' > clean.html
